4e768081e12b969e3e8c9b4bfbfe3a01beccaec8
Juan Pablo Tessore <juanpablo.tessore@gmail.com>
2026-03-31 19:34:41 -0300

    update proyect files

--- src/Generador.java
+++ src/Generador.java
@@ -1,9 +1,6 @@
 
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
  * Genera MiLexico.java a partir de lexico.flex usando JFlex.
@@ -12,26 +9,7 @@ import java.util.Scanner;
 public class Generador {
 
     public static void main(String[] args) {
-
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("=== Analizador Léxico ===");
-        System.out.println("¿Desde dónde desea leer?");
-        System.out.println("  1 - Desde lexico.flex");
-        System.out.println("  2 - Desde lexico_indentacion.flex");
-        System.out.print("Ingrese su opción: ");
-
-        String opcion = teclado.nextLine().trim();
-        teclado.close();
-        String path;
-        if (opcion.equals("1")) {
-            path = "./src/lexico.flex";
-        } else if (opcion.equals("2")) {
-            path = "./src/lexico_indentacion.flex";
-        } else {
-            System.err.println("Opción inválida. Saliendo.");
-            return;
-        }
+        String path = "./src/lexico.flex";
         System.out.println("Generando léxico desde: " + path);
         File file = new File(path);
         jflex.generator.LexGenerator generator = new jflex.generator.LexGenerator(file);
